57a9b1e705be09ba07d490bed1f7320db1bcd487
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 13:24:53 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-"""
-create a variable app, instance of Flask
-"""
-
-from flask import Flask, jsonify
+""" status for api """
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
+from flask import jsonify
 import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 
-@app.route("/")
-def hello():
-    return ("hello")
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_appcontext(exception):
+    """Closes the database connection at the end of the request."""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_error(error):
+def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", 5000)
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)

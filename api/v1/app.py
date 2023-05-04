95ef3418c40fddcc0f055bef9be3b22a251b1a93
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 12:58:53 +0100

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,10 @@ def teardown_appcontext(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))

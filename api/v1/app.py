7a378a10f0b49e8b54cae1f6caa7375a361f985f
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 13:31:55 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)

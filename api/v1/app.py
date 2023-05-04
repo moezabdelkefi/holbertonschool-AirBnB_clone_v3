a9582193329d464f4d7f500b1a39e6e4ec61b090
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 14:17:06 +0100

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext

f5e6f3329f76ff179d0951ac8cc8e7c48d0484bf
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 12:31:21 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Closes the database connection at the end of the request."""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")

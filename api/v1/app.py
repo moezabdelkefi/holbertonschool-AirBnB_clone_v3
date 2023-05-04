b96a5456fde15b7a259f44e135614bf8c76b076c
moezabdelkef <moezabdelkefi17@gmail.com>
2023-05-04 14:09:37 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Closes the database connection at the end of the request."""
     storage.close()
+
 
 if __name__ == "__main__":
     import os
